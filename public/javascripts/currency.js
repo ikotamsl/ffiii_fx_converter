f5eade7168b85a7b1b4bc3d9cfbf2579c0efd2a0
ikotamsl <ikotamsl@gmail.com>
2024-11-23 18:45:51 +0500

    formatting + checking for enabled currency api

--- public/javascripts/currency.js
+++ public/javascripts/currency.js
@@ -1,48 +1,53 @@
-const https = require("https");
+const https = require('https');
+const debug = require('debug')('currency-api');
+
+const apiEnabled = process.env.EXCHANGE_API_ENABLED === 'true';
 
 const currencyExchange = async (from, to) => {
-  if (!process.env.EXCHANGE_API_ENABLED) {
-    console.log(
-      "Exchange API is not enabled, conversion should be done manually"
-    );
-    return null;
-  }
+	debug(`Exchange API enabled: ${apiEnabled}`);
 
-  try {
-    const options = {
-      hostname: `v6.exchangerate-api.com`,
-      path: `/v6/${process.env.EXCHANGE_API_KEY}/pair/${from}/${to}`,
-      method: "GET",
-      headers: {
-        Connection: "keep-alive"
-      }
-    };
+	try {
+		if (!apiEnabled) {
+			console.log(
+				'Exchange API is not enabled, conversion should be done manually'
+			);
+			return null;
+		}
 
-    const body = await new Promise((resolve, reject) => {
-      const req = https.request(options, (res) => {
-        let chunks = "";
+		const options = {
+			hostname: `v6.exchangerate-api.com`,
+			path: `/v6/${process.env.EXCHANGE_API_KEY}/pair/${from}/${to}`,
+			method: 'GET',
+			headers: {
+				Connection: 'keep-alive'
+			}
+		};
 
-        res.on("data", (chunk) => {
-          chunks += chunk;
-        });
+		const body = await new Promise((resolve, reject) => {
+			const req = https.request(options, (res) => {
+				let chunks = '';
 
-        res.on("end", () => {
-          resolve(JSON.parse(chunks));
-        });
-      });
+				res.on('data', (chunk) => {
+					chunks += chunk;
+				});
 
-      req.on("error", (error) => {
-        reject(error);
-      });
+				res.on('end', () => {
+					resolve(JSON.parse(chunks));
+				});
+			});
 
-      req.end();
-    });
+			req.on('error', (error) => {
+				reject(error);
+			});
 
-    return body;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
+			req.end();
+		});
+
+		return body;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 };
 
 module.exports = { currencyExchange };
